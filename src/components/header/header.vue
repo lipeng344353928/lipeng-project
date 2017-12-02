a052677e256a35883f43183021991a15b232b345
lp344353928 <344353928@qq.com>
2017-12-02 13:28:02 +0800

    first commit

---
+++ src/components/header/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+      我是Header
+  </div>
+</template>
+
+<script type="text/ecmascript6">
+    export default {
+        components: {
+            
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
